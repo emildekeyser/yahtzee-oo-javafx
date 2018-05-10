f0996449c7c0a143a6d7986bb884273f653ba776
emil.dekeyser@student.ucll.be <emil.dekeyser@student.ucll.be>
2018-05-10 17:36:38 +0200

    model sprint 1 klaar

---
+++ OOO_Part2/yahtzee/src/model/domain/Dice.java
@@ -0,0 +1,41 @@
+package model.domain;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Dice
+{
+	private int values[];
+	
+	public Dice()
+	{
+		this.values = new int[] {0,0,0,0,0};
+	}
+	
+	public void roll(boolean[] flags)
+	{
+		if(flags.length != values.length || flags == null)
+		{
+			this.roll();
+		}
+		else
+		{
+			for (int i = 0; i < values.length; i++)
+			{
+				if (flags[i])
+				{
+					this.values[i] = ThreadLocalRandom.current().nextInt(1, 7); // 1 t.e.m. 6
+				}
+			}
+		}
+	}
+
+	public int[] getValues()
+	{
+		return this.values;
+	}
+
+	public void roll()
+	{
+		 this.roll(new boolean[]{true, true, true, true, true});
+	}
+}
--- OOO_Part2/yahtzee/src/model/domain/Yahtzee.java
+++ OOO_Part2/yahtzee/src/model/domain/Yahtzee.java
@@ -2,9 +2,14 @@ package model.domain;
 
 public class Yahtzee extends Game
 {
+	Dice currentDice;
+	int rollCounter;
+	
 	public Yahtzee(PlayerListing players)
 	{
 		super(players);
+		this.currentDice = new Dice();
+		this.rollCounter = 0;
 	}
 
 	@Override
@@ -12,5 +17,24 @@ public class Yahtzee extends Game
 	{
 		return super.players.activePlayer();
 	}
+	
+	public void roll(boolean[] rerolFlags)
+	{
+		if(this.rollCounter > 3)
+		{
+			throw new DomainException("Men mag maar driemaal rollen.");
+		}
+		else if(this.rollCounter < 1)
+		{
+			this.currentDice.roll();
+		}
+		else 
+		{
+			this.currentDice.roll(rerolFlags);
+			this.rollCounter++;
+		}
+	}
+	
+	
 
 }
